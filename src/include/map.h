e4a0b8d56d3779bb569fede45fddc3b98d0503f2
Michael <mzill@t-online.de>
2019-03-13 09:28:23 +0100

    namespace fixes

--- src/include/map.h
+++ src/include/map.h
@@ -6,8 +6,6 @@
 #include "json.hpp"
 #include "spline.h"
 
-using namespace std;
-using namespace tk;
 
 struct Point {
   double x;
@@ -23,28 +21,28 @@ class Map {
 
   Config &cfg_;
 
-  spline spline_x_;
-  spline spline_y_;
-  spline spline_dx_;
-  spline spline_dy_;
+  tk::spline spline_x_;
+  tk::spline spline_y_;
+  tk::spline spline_dx_;
+  tk::spline spline_dy_;
 
   // Load up map values for waypoint's x,y,s and d normalized normal vectors
-  vector<double> map_waypoints_x_;
-  vector<double> map_waypoints_y_;
-  vector<double> map_waypoints_s_;
-  vector<double> map_waypoints_dx_;
-  vector<double> map_waypoints_dy_;
-  vector<double> map_s_;
+  std::vector<double> map_waypoints_x_;
+  std::vector<double> map_waypoints_y_;
+  std::vector<double> map_waypoints_s_;
+  std::vector<double> map_waypoints_dx_;
+  std::vector<double> map_waypoints_dy_;
+  std::vector<double> map_s_;
 
   // high resultion map 1 point per meter
-  vector<double> high_res_map_waypoints_x_;
-  vector<double> high_res_map_waypoints_y_;
-  vector<double> high_res_map_waypoints_dx_;
-  vector<double> high_res_map_waypoints_dy_;
-  vector<double> high_res_map_waypoints_s_;
+  std::vector<double> high_res_map_waypoints_x_;
+  std::vector<double> high_res_map_waypoints_y_;
+  std::vector<double> high_res_map_waypoints_dx_;
+  std::vector<double> high_res_map_waypoints_dy_;
+  std::vector<double> high_res_map_waypoints_s_;
 
-  vector<PointIdx> high_res_map_waypoints_sorted_x_;
-  vector<PointIdx> high_res_map_waypoints_sorted_y_;
+  std::vector<PointIdx> high_res_map_waypoints_sorted_x_;
+  std::vector<PointIdx> high_res_map_waypoints_sorted_y_;
 
  public:
   Map(Config &cfg) : cfg_(cfg){};
@@ -52,14 +50,14 @@ class Map {
 
   void LoadData(string &filename);
 
-  vector<double> getXY(double s, double d);
-  vector<double> getXY2(double s, double d);
-  vector<double> getXYspline(double s, double d);
+  std::vector<double> getXY(double s, double d);
+  std::vector<double> getXY2(double s, double d);
+  std::vector<double> getXYspline(double s, double d);
   double distance(double x1, double y1, double x2, double y2);
   int ClosestWaypoint(double x, double y);
   int ClosestWaypoint2(double x, double y);
   int NextWaypoint(double x, double y, double theta);
   XYPoints makePath(JMT jmt_s, JMT jmt_d, const double t, const int n);
-  vector<double> getFrenet(double x, double y, double theta);
+  std::vector<double> getFrenet(double x, double y, double theta);
   double deg2rad(double x) { return x * M_PI / 180; };
 };
